4f496e6e8673e605cc8d0e37c50a5210ccde5ac2
Samosky9 <haryanwolesamuel27@gmail.com>
2023-11-03 06:30:09 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,9 @@
-curl -Lso "hidden_4.pyc" "https://github.com/alx-tools/0x02.py/raw/master/hidden_4.pyc"
+#!/usr/bin/python3
+import hidden_4
+if __name__ == "__main__":
+    list = []
+    for i in dir(hidden_4):
+        if i[0] != '_':
+            list.append(i)
+            for i in list:
+                print(i)
