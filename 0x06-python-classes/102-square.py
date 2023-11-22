b1e1c87b3987fbdc86c87a841aeb8984e15c91ac
Samosky9 <haryanwolesamuel27@gmail.com>
2023-11-21 20:47:39 -0800

    102-square.py

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,82 +1,55 @@
 #!/usr/bin/python3
-"""Python Module(Squares)"""
+""" This module defines a square class with a private size attribute with
+    a setter and getter methods to retrieve and update the size attribute.
+    Square also has a public instance method area() that
+    gets the area of the square """
+
 
 class Square:
-        def __init__(self, size=0):
-        
-        self.size = size
+    """ A square class with a private instance size attribute """
+
+    def __init__(self, size=0):
+        """ Initializes self """
+        self.__size = size
+
+    def __eq__(self, other):
+        """ Comparing instances """
+        return self.area() == other.area()
+
+    def __lt__(self, other):
+        """ Comparing instances """
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """ Comparing instances """
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        """ Comparing instances """
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """ Comparing instances """
+        return self.area() >= other.area()
+
+    def __ne__(self, other):
+        """ Comparing instances """
+        return self.area() != other.area()
 
     @property
     def size(self):
-        
+        """ Returns the size of the square """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        """
+        """ Sets the size to value """
         if not isinstance(value, int):
-            raise TypeError('size must be an integer')
-        else:
-            if value < 0:
-                raise ValueError('size must be >= 0')
-            else:
-                self.__size = value
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
-        
-        return self.size ** 2
-
-    def __eq__(self, value):
-        
-        if isinstance(value, Square):
-            return self.area() == value.area()
-        else:
-            return False
-
-    def __ne__(self, value):
-        
-        if isinstance(value, Square):
-            return self.area() != value.area()
-        else:
-            return True
-
-    def __gt__(self, value):
-        
-        if isinstance(value, Square):
-            return self.area() > value.area()
-        else:
-            err_msg = "'>' not supported between instances of 'Square' and"
-            val_type = str(type(value)).split("'")[1]
-            raise TypeError("{} '{}'".format(err_msg, val_type))
-            return False
-
-    def __ge__(self, value):
-       
-        if isinstance(value, Square):
-            return self.area() >= value.area()
-        else:
-            err_msg = "'>=' not supported between instances of 'Square' and"
-            val_type = str(type(value)).split("'")[1]
-            raise TypeError("{} '{}'".format(err_msg, val_type))
-            return False
-
-    def __lt__(self, value):
-       
-        if isinstance(value, Square):
-            return self.area() < value.area()
-        else:
-            err_msg = "'<' not supported between instances of 'Square' and"
-            val_type = str(type(value)).split("'")[1]
-            raise TypeError("{} '{}'".format(err_msg, val_type))
-            return False
-
-    def __le__(self, value):
-
-        if isinstance(value, Square):
-            return self.area() <= value.area()
-        else:
-            err_msg = "'<=' not supported between instances of 'Square' and"
-            val_type = str(type(value)).split("'")[1]
-            raise TypeError("{} '{}'".format(err_msg, val_type))
-            return False
+        """ Returns the area of the square """
+        return self.__size * self.__size
