5e33d70c4509aa7b348c1a77b663723a053aa31c
Samosky9 <haryanwolesamuel27@gmail.com>
2023-11-30 13:40:12 -0800

    updated

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+cked_class.py 
 def magic_string(string=[]):
-        string.append("BestSchool")
-            return ", ".join(string)
+    string.append("BestSchool")
+    return ", ".join(string)
